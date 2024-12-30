0e226c70125065c23288e0b6fae34a6248f4d07f
Lakshay Bansal <lakshay.bansal198@gmail.com>
2024-12-30 22:00:38 +0530

    feat: implement socket.io server for real-time messaging and enhance app structure

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express"
-import  {dbConnect} from './utils/database.js'
+import { dbConnect } from './utils/database.js'
 import dotenv from "dotenv"
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
-import { createUser } from "./seeders/user.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { NEW_MESSAGE } from "./constants/event.js";
+import { v4 as uuid } from "uuid"
 
 
 //importing the routes
@@ -20,15 +23,26 @@ const adminSecretKey = process.env.ADMIN_SECRET_KEY.trim() || "admin"
 
 
 dotenv.config({
-    path:"./.env"
+    path: "./.env"
 });
 
+//creating the express app
 const app = express();
 
+//creating the http server
+const server = createServer(app);
+
+//creating the socket server
+const io = new Server(server, {
+    cors: {
+        origin: "*", //allowing all the origins
+        credentials: true //allowing credentials means allowing cookies
+    }
+})
 
 //using middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
@@ -48,10 +62,56 @@ dbConnect(process.env.MONGO_URI);
 
 
 //mounting the router
-app.use("/user" , userRoutes);
-app.use("/chat" , chatRoutes);
-app.use("/admin" , adminRoutes);
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/admin", adminRoutes);
 
+
+
+app.get("/", (req, resp) => {
+    resp.send("Hello World");
+})
+
+
+//socket connection
+io.on("connection", (socket) => {
+
+    const user = {
+        _id:"dbnubdowqbdw",
+        name:"Lakshay"
+    }
+
+    console.log("User connected : ", socket.id);
+
+    //listening for new message event i.e. when a new message is sent by the client then this event will be emitted
+    socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+
+        const messageForRealTime = {
+            content: message, 
+            _id: uuid(),   
+            sender: {
+                _id: user._id,  
+                name: user.name,
+            },
+            chat: chatId,
+            createdAt: new Date().toISOString()
+        }
+
+        const messageForDB = {
+            content: message, 
+            sender: user._id,
+            chat: chatId,
+        }
+
+        console.log("Message : ", messageForRealTime);
+
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected : ", socket.id);
+    })
+
+})
 
 
 //using error middleware at last so that it can catch all the errors that are thrown in the application
@@ -60,9 +120,28 @@ app.use(errorMiddleware)
 
 
 const port = process.env.PORT || 3000;
-app.listen(port , ()=>{
+server.listen(port, () => {
     console.log(`Server is running on port ${port} in ${node_env} mode`);
 })
 
 
-export {node_env , adminSecretKey};
+export { node_env, adminSecretKey };
+
+
+
+
+
+
+//socket.io main concepts
+// 1. connection - when a user connects to the server
+// 2. disconnect - when a user disconnects from the server
+// 3. emit - sending data from the server to the client i.e. sending data
+// 4. on - listening for the data from the client i.e. receiving data and performing some action on it
+// 5. broadcast - sending data to all the clients connected to the server except the sender
+// 6. rooms - creating a separate room for the clients to join and communicate with each other
+// 7. join - joining a room
+// 8. leave - leaving a room
+// 9. to - sending data to a specific room
+// 10. in - sending data to a specific room
+// 11. of - creating a namespace i.e. creating a separate connection for the clients
+// 12. disconnecting - disconnecting a client from the server
