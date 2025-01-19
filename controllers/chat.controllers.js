92d88c90a725094c5e66760e5b2b436606cb3810
Lakshay Bansal <lakshay.bansal198@gmail.com>
2025-01-19 13:14:24 +0530

    fix : including the chatId on emitting the ALERT event

--- controllers/chat.controllers.js
+++ controllers/chat.controllers.js
@@ -31,7 +31,10 @@ const newGroupChatController = TryCatch(
         // console.log(creator);
 
         //emitting the event to all the members when the group chat is created 
-        emitEvent(req, ALERT, allMembers, `Welcome to the group chat ${name} created by ${creator.name}`);
+        emitEvent(req, ALERT, allMembers, {
+            message: `Welcome to the group chat ${name} created by ${creator.name}`,
+            chatId: groupChatDetails._id
+        });
 
         //emitting the event to only the members of the group chat
         emitEvent(req, REFETCH_CHATS, members);
@@ -158,7 +161,10 @@ const addGroupMembersController = TryCatch(
 
         const allUsersName = allNewMembers.map(({ name }) => name).join(", ");
 
-        emitEvent(req, ALERT, chat.members, `${allUsersName} added to the group ${chat.name}`);
+        emitEvent(req, ALERT, chat.members, {
+            message: `${allUsersName} added to the group ${chat.name}`,
+            chatId
+        });
         emitEvent(req, REFETCH_CHATS, chat.members);
 
 
@@ -230,7 +236,10 @@ const removeGroupMemberController = TryCatch(
 
 
         //emit the event to all the members of the chat
-        emitEvent(req, ALERT, chat.members, `${removedUser.name} removed from the group ${chat.name}`);
+        emitEvent(req, ALERT, chat.members, {
+            message: `${removedUser.name} removed from the group ${chat.name}`,
+            chatId
+        });
 
         //refetch the chats
         emitEvent(req, REFETCH_CHATS, allChatMembers);
@@ -261,31 +270,31 @@ const leaveGroupChatController = TryCatch(
         const chat = await Chat.findById(chatId);
 
         //if chat is not found then throw an error
-        if(!chat){
+        if (!chat) {
             return next(new ErrorHandler("Chat not found", 404));
         }
 
         //if chat is not a group chat then throw an error
-        if(!chat.groupChat){
+        if (!chat.groupChat) {
             return next(new ErrorHandler("This is not a group chat", 400));
         }
 
         //if user is not in the group chat
-        if(!chat.members.includes(req.user.toString())){
-            return next(new ErrorHandler("You are not added in the group" , 400));
+        if (!chat.members.includes(req.user.toString())) {
+            return next(new ErrorHandler("You are not added in the group", 400));
         }
 
         //finding reamining members in the chat
-        const remainingMembers = chat.members.filter((member)=>{
+        const remainingMembers = chat.members.filter((member) => {
             return member.toString() !== req.user.toString();
         })
 
-        if(remainingMembers.length < 3){
+        if (remainingMembers.length < 3) {
             return next(new ErrorHandler("Group must have atleast 3 members", 400));
         }
 
         //if the user is the creator of the chat
-        if(chat.creator.toString() === req.user.toString()){
+        if (chat.creator.toString() === req.user.toString()) {
 
             //if the creator is leaving the chat then assign the new creator to the chat 
             //randomly from the remaining members
@@ -301,17 +310,20 @@ const leaveGroupChatController = TryCatch(
 
         //saving the chat and finding the user who is leaving the chat
         const [user] = await Promise.all([
-            User.findById(req.user , "name"),
+            User.findById(req.user, "name"),
             chat.save()
-         ]);
+        ]);
 
         //emit the event to all the members of the chat
-        emitEvent(req, ALERT, chat.members, `${user.name} left the group ${chat.name}`);
+        emitEvent(req, ALERT, chat.members, {
+            message: `${user.name} left the group ${chat.name}`,
+            chatId
+        });
 
 
         return resp.status(200).json({
-            success:true,
-            message:"Left the group chat successfully"
+            success: true,
+            message: "Left the group chat successfully"
         })
 
     }
@@ -322,16 +334,16 @@ const leaveGroupChatController = TryCatch(
 
 //send attachment controller
 const sendAttachmentController = TryCatch(
-    async (req , resp , next)=>{
+    async (req, resp, next) => {
 
-        const {chatId} = req.body;
+        const { chatId } = req.body;
 
-        const [chat , user] = await Promise.all([
+        const [chat, user] = await Promise.all([
             Chat.findById(chatId),
-            User.findById(req.user , "name avatar")
+            User.findById(req.user, "name avatar")
         ]);
 
-        if(!chat){
+        if (!chat) {
             return next(new ErrorHandler("Chat not found", 404));
         }
 
@@ -350,23 +362,23 @@ const sendAttachmentController = TryCatch(
 
         //message for real time 
         const messageRealTime = {
-            sender : {
-                _id:user._id,
-                name:user.name,
-                avatar:user.avatar.url  //optional but can be included
+            sender: {
+                _id: user._id,
+                name: user.name,
+                avatar: user.avatar.url  //optional but can be included
             },
-            chat : chatId,
+            chat: chatId,
             attachments,
-            content:"",
+            content: "",
         }
 
 
         //message for the db
         const messageDb = {
-            sender : req.user,
-            chat : chatId,
+            sender: req.user,
+            chat: chatId,
             attachments,
-            content:"",
+            content: "",
         }
 
         //saving the message to the db
@@ -375,16 +387,16 @@ const sendAttachmentController = TryCatch(
         console.log(chat.members);
 
         //emitting the new attachment event to all the members of the chat
-        emitEvent(req , NEW_MESSAGE , chat.members , {
-            message:messageRealTime,
+        emitEvent(req, NEW_MESSAGE, chat.members, {
+            message: messageRealTime,
             chatId
         });
 
         //emitting the new message alert to all the members of the chat
-        emitEvent(req , NEW_MESSAGE_ALERT , chat.members , {chatId});
+        emitEvent(req, NEW_MESSAGE_ALERT, chat.members, { chatId });
 
         return resp.status(200).json({
-            success:true,
+            success: true,
             message
         })
     }
@@ -393,44 +405,44 @@ const sendAttachmentController = TryCatch(
 
 //get messages controller
 const getMessagesController = TryCatch(
-    async (req , resp , next)=>{
+    async (req, resp, next) => {
 
         const chatId = req.params.id;
 
-        const {page = 1} = req.query;
-        
-        const result_per_page = 20 ;
-        
-        const skip = (page-1)*result_per_page;
+        const { page = 1 } = req.query;
+
+        const result_per_page = 20;
+
+        const skip = (page - 1) * result_per_page;
 
         const chat = await Chat.findById(chatId);
 
-        if(!chat){
+        if (!chat) {
             return next(new ErrorHandler("Chat not found", 404));
         }
 
-        if(!chat.members.includes(req.user.toString())){
+        if (!chat.members.includes(req.user.toString())) {
             return next(new ErrorHandler("You are not a member of the chat", 403));
         }
 
-        const [messages , totalMessagesCount ] = await Promise.all([
+        const [messages, totalMessagesCount] = await Promise.all([
 
-            Message.find({chat:chatId})
-            .sort({createdAt:-1})   //sorting the messages in descending order of createdAt. Latest show first
-            .skip(skip) //skip the messages that are already shown in the previous page
-            .limit(result_per_page) //how many messages to be shown in a page
-            .populate("sender" , "name avatar") //populate the sender field with name and avatar
-            .lean() , //lean method to convert the mongoose document to plain javascript object
+            Message.find({ chat: chatId })
+                .sort({ createdAt: -1 })   //sorting the messages in descending order of createdAt. Latest show first
+                .skip(skip) //skip the messages that are already shown in the previous page
+                .limit(result_per_page) //how many messages to be shown in a page
+                .populate("sender", "name avatar") //populate the sender field with name and avatar
+                .lean(), //lean method to convert the mongoose document to plain javascript object
 
-            Message.countDocuments({chat:chatId}) //count the total number of messages in the chat
-            
+            Message.countDocuments({ chat: chatId }) //count the total number of messages in the chat
+
         ])
 
         const totalPages = Math.ceil(totalMessagesCount / result_per_page) || 0;  //total number of pages required to show all the messages
 
         return resp.status(200).json({
-            success:true,
-            messages:messages.reverse(),
+            success: true,
+            messages: messages.reverse(),
             totalPages
         })
 
@@ -441,41 +453,41 @@ const getMessagesController = TryCatch(
 
 //get chat details 
 const getChatDetailsController = TryCatch(
-    async (req , resp , next)=>{
+    async (req, resp, next) => {
 
-        if(req.query.populate === "true"){
+        if (req.query.populate === "true") {
 
             //lean method is used to convert the mongoose document to plain javascript object
             const chat = await Chat.findById(req.params.id).populate("members", "name avatar").lean();
 
-            if(!chat){
+            if (!chat) {
                 return next(new ErrorHandler("Chat not found", 404));
             }
 
             //without lean method we cannot modify the object unless we convert it to plain javascript object or change in the db
-            chat.members = chat.members.map(({_id , name , avatar})=>{
-                return { 
+            chat.members = chat.members.map(({ _id, name, avatar }) => {
+                return {
                     _id,
                     name,
-                    avatar:avatar.url
+                    avatar: avatar.url
                 }
             })
 
             return resp.status(200).json({
-                success:true,
+                success: true,
                 chat
             })
 
-        } else{
+        } else {
 
             const chat = await Chat.findById(req.params.id);
 
-            if(!chat){
+            if (!chat) {
                 return next(new ErrorHandler("Chat not found", 404));
             }
 
             return resp.status(200).json({
-                success:true,
+                success: true,
                 chat
             })
 
@@ -486,43 +498,43 @@ const getChatDetailsController = TryCatch(
 
 //rename the group chat controller
 const renameGroupChatController = TryCatch(
-    async (req , resp , next)=>{
+    async (req, resp, next) => {
 
         const chatId = req.params.id;
-        const {name} = req.body;
+        const { name } = req.body;
 
-        if(!name){
+        if (!name) {
             return next(new ErrorHandler("Please provide name", 400));
         }
 
         const chat = await Chat.findById(chatId);
 
-        if(!chat){
+        if (!chat) {
             return next(new ErrorHandler("Chat not found", 404));
         }
 
-        if(!chat.groupChat){
+        if (!chat.groupChat) {
             return next(new ErrorHandler("This is not a group chat", 400));
         }
 
         //creator and admins can rename the chat
-        if(chat.creator.toString() !== req.user.toString() && !chat.admins.includes(req.user.toString())){
+        if (chat.creator.toString() !== req.user.toString() && !chat.admins.includes(req.user.toString())) {
             return next(new ErrorHandler("You are not authorized to rename the chat", 403));
         }
 
         //updating the name of the chat
         chat.name = name;
 
-        
+
         //saving the chat
         await chat.save();
 
         //emit the refetch chat event to all the members of the chat
-        emitEvent(req , REFETCH_CHATS , chat.members);
+        emitEvent(req, REFETCH_CHATS, chat.members);
 
         return resp.status(200).json({
-            success:true,
-            message:"Group renamed successfully"
+            success: true,
+            message: "Group renamed successfully"
         })
 
     }
@@ -531,7 +543,7 @@ const renameGroupChatController = TryCatch(
 
 //delete chat controller
 const deleteChatController = TryCatch(
-    async (req , resp , next)=>{
+    async (req, resp, next) => {
 
         const chatId = req.params.id;
 
@@ -539,17 +551,17 @@ const deleteChatController = TryCatch(
         const chat = await Chat.findById(chatId);
 
         //if chat is not found then throw an error
-        if(!chat){
+        if (!chat) {
             return next(new ErrorHandler("Chat not found", 404));
         }
 
         const members = chat.members;
 
-        if(chat.groupChat &&  chat.creator.toString() !== req.user.toString()){
+        if (chat.groupChat && chat.creator.toString() !== req.user.toString()) {
             return next(new ErrorHandler("Only the creator can delete the group chat", 403));
         }
 
-        if(!chat.groupChat && !chat.members.includes(req.user.toString())){
+        if (!chat.groupChat && !chat.members.includes(req.user.toString())) {
             return next(new ErrorHandler("You are not authorized to delete the chat", 403));
         }
 
@@ -557,15 +569,15 @@ const deleteChatController = TryCatch(
 
         //finding all the messages with attachments in the chat
         const messageWithAttachments = await Message.find({
-            chat:chatId,
-            attachments:{$exists:true , $ne:[]} //attachments exists and not qual to empty array
+            chat: chatId,
+            attachments: { $exists: true, $ne: [] } //attachments exists and not qual to empty array
         });
 
         const public_ids = [];
 
         //pushing all the public ids of the attachments of all the messages in the chat to the public_ids array
-        messageWithAttachments.forEach(({attachments})=>{
-            attachments.forEach(({ public_id })=>{
+        messageWithAttachments.forEach(({ attachments }) => {
+            attachments.forEach(({ public_id }) => {
                 public_ids.push(public_id);
             })
         })
@@ -574,16 +586,16 @@ const deleteChatController = TryCatch(
         await Promise.all([
             deleteFileFromCloudinary(public_ids),
             chat.deleteOne(),
-            Message.deleteMany({chat:chatId})
+            Message.deleteMany({ chat: chatId })
         ])
 
         //emitting the event
-        emitEvent(req , REFETCH_CHATS , members);
+        emitEvent(req, REFETCH_CHATS, members);
 
         //returning the response
         return resp.status(200).json({
-            success:true,
-            message:"Chat deleted successfully"
+            success: true,
+            message: "Chat deleted successfully"
         })
 
 
@@ -597,10 +609,10 @@ const makeUserAdminController = TryCatch(
     async (req, resp, next) => {
 
         //fetching the chatId from params
-        const { chatId } = req.params;
+        const chatId = req.params.id;
 
         //fetching the required data
-        const {userId} = req.body;
+        const { userId } = req.body;
 
         //finding the chat
         const [chat, user] = await Promise.all([
@@ -640,7 +652,10 @@ const makeUserAdminController = TryCatch(
         await chat.save();
 
         //emitting the event
-        emitEvent(req, ALERT, chat.members, `${user.name} is now an group admin`);
+        emitEvent(req, ALERT, chat.members, {
+            message: `${user.name} is now an admin`,
+            chatId
+        });
         emitEvent(req, REFETCH_CHATS, chat.members);
 
         //return the response
@@ -650,7 +665,7 @@ const makeUserAdminController = TryCatch(
         });
 
     }
-    
+
 )
 
 
